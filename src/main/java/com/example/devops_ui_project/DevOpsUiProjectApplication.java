6671e3348ef11a6589e8929e6d689e743bf21894
SkyHighSpirits <rawsomegamesinfo@gmail.com>
2023-02-24 15:00:45 +0100

    Project Structure

---
+++ src/main/java/com/example/devops_ui_project/DevOpsUiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.devops_ui_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsUiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevOpsUiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devops_ui_project/DevOpsUiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops_ui_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsUiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
